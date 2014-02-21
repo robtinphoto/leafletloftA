645134f507524b4049eb82d3fb60b4f331d921e6
robtinphoto <robtinphoto@gmail.com>
2014-02-20 20:17:12 -0700

    First commit
    first time

---
+++ leafletloftA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(leafletloftA.Startup))]
+namespace leafletloftA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
